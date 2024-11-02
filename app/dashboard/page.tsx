9331e0967873b8947f59fea21e7afbac101f568d
Lucas Class <lucasclass94@gmail.com>
2024-11-01 23:24:16 -0300

    add(app/dashboard)  creatind the dashboard path

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <span>projects will be placed here for display and control testing.</span>
+    </div>
+  );
+}
